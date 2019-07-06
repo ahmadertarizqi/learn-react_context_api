1084e1c74c6da6ded3ce4a8eedd47c204915a8a7
ahmadertarizqi <ahmadderta.rizqi30@gmail.com>
2019-07-07 02:27:32 +0700

    Updates

---
+++ precache-manifest.6eacf9d72dc9018a4fbaa44f55e3d43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "322ab16723bc7e7f46fd",
+    "url": "/learn-react_context_api/static/js/runtime~main.477e3bdd.js"
+  },
+  {
+    "revision": "e4f0b1344b1971ef2072",
+    "url": "/learn-react_context_api/static/js/main.d047fe40.chunk.js"
+  },
+  {
+    "revision": "1fce60e5aa9474d21bba",
+    "url": "/learn-react_context_api/static/js/2.3dcb54ed.chunk.js"
+  },
+  {
+    "revision": "e4f0b1344b1971ef2072",
+    "url": "/learn-react_context_api/static/css/main.e4ec81fe.chunk.css"
+  },
+  {
+    "revision": "1fce60e5aa9474d21bba",
+    "url": "/learn-react_context_api/static/css/2.234654b4.chunk.css"
+  },
+  {
+    "revision": "0e8d940c49b06d3b598968c5bd3c502c",
+    "url": "/learn-react_context_api/index.html"
+  }
+];
